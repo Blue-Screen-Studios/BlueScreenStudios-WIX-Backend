cfc2d51336f7c2e845d88c1047138b8c16322ca0
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-10-25 16:35:15 -0500

    Update http-functions.js

--- backend/http-functions.js
+++ backend/http-functions.js
@@ -2,22 +2,52 @@ import { ok, notFound, serverError } from 'wix-http-functions';
 import wixdata from 'wix-data';
 
 export function get_users(request) {
-    
-    let options = {
+
+    let PlayFabID;
+
+    if(request.query)
+    {
+        PlayFabID = String(request.query.playfab_id);
+    }
+
+    let response = {
         "headers": {
             "Content-Type": "application/json"
         }
     };
 
-    return wixdata.query("users")
+    let userQuery = wixdata.query("users");
+    
+    if(PlayFabID)
+    {
+        userQuery = userQuery.eq("playfab_id", PlayFabID);
+    }
+
+    return userQuery
         .find()
         .then(results => {
-            if(results.items.length > 0) {
-                options.body = {
+            if (results.items.length > 0) {
+                response.body = {
+                    "status": 200,
                     "items": results.items
-                }
+                };
 
-                return ok(options);
+                return ok(response);
             }
+
+            response.body = {
+                "status": 404,
+                "error": "No response object could be found with the specified filters."
+            };
+
+            return notFound(response);
+        })
+        .catch((error) => {
+            response.body = {
+                "status": 500,
+                "error": error
+            };
+
+            return serverError(response);
         })
 }
